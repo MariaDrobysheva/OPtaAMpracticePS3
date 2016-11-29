948b71e03dacc6d7f7ebe69a4f98427b05b10865
Maria <Masha.drobysheva.99@mail.ru>
2016-11-29 13:15:12 +0200

    day 1

---
+++ day 1__51.cpp
@@ -0,0 +1,39 @@
+//Програма, що видаляє зайві пробіли
+
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+ 
+ 
+int main()
+{
+    string buf;
+ 
+    getline(cin, buf);
+    
+ 
+    bool space_sleep = false;
+    string res;
+    for(int i=0; i<buf.length(); i++)
+    {
+        if(buf[i]==' ' && space_sleep==true)
+        {
+            continue;
+        }
+        if(buf[i]==' ' && space_sleep==false)
+        {
+            res.push_back(buf[i]);
+            space_sleep = true;
+        }
+        else
+        {
+            res.push_back(buf[i]);
+            space_sleep = false;
+        }
+    }
+    cout<<res;
+ system ("Pause");
+    return 0;
+ 
+}
